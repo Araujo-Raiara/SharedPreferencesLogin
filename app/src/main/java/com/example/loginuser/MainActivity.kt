9eb30acffd2685011c6bd64ac6db8b8238e3d827
Mauro Crispim <mcrispim@hotmail.com>
2023-03-25 16:25:11 -0300

    Added funcionality to the Cancel button

--- app/src/main/java/com/example/loginuser/MainActivity.kt
+++ app/src/main/java/com/example/loginuser/MainActivity.kt
@@ -1,11 +1,18 @@
 package com.example.loginuser
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val buttonNewUser = findViewById<Button>(R.id.new_user_btn)
+        buttonNewUser.setOnClickListener {
+            startActivity(Intent(applicationContext, RegisterActivity::class.java))
+        }
     }
 }
