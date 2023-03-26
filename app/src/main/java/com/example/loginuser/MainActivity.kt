af998baaf33d189eb4c8cd714fb50875c9249cd4
Mauro Crispim <mcrispim@hotmail.com>
2023-03-25 23:11:37 -0300

    implementa cadastrar e testar senha

--- app/src/main/java/com/example/loginuser/MainActivity.kt
+++ app/src/main/java/com/example/loginuser/MainActivity.kt
@@ -4,11 +4,26 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val userLogin = findViewById<EditText>(R.id.user_login)
+        val password = findViewById<EditText>(R.id.password_user)
+
+        val buttonLogin = findViewById<Button>(R.id.login_btn)
+        buttonLogin.setOnClickListener {
+            if (userLogin.text.toString() == prefs.username &&
+                password.text.toString() == prefs.password
+            ) {
+                Toast.makeText(this, "Sucesso!", Toast.LENGTH_LONG).show()
+                startActivity(Intent(applicationContext, RegisterActivity::class.java))
+            } else {
+        }
 
         val buttonNewUser = findViewById<Button>(R.id.new_user_btn)
         buttonNewUser.setOnClickListener {
